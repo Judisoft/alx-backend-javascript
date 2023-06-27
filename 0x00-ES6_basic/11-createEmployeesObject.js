1aa4d72a695ceeb7a48d52a9834a8f08d9201bc3
KUM JUDE <kumjude09@gmail.com>
2023-06-27 03:24:22 +0100

    implemented iterator

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,7 @@
+export default function createEmployeesObject(departmentName, employees) {
+    const $departmentName = arguments[0];
+    const $employees = arguments[1];
+    return {
+        departmentName: employees
+    };
+}
