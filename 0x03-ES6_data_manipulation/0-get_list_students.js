7472dd66e0eff20220bc8b3896baf0954c947f11
Judisoft <kumjude09@gmail.com>
2023-07-03 17:57:00 -0700

    function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+    const objects = [
+        {
+            id: 1,
+            firstName: 'Guillaume',
+            location: 'Francisco'
+        },
+        {
+            id: 2,
+            firstName: 'James',
+            location: 'Columbia'
+        },
+        {
+            id: 5,
+            firstName: 'Serena',
+            location: 'San Francisco'
+        }
+    ];
+
+    return objects;
+}
